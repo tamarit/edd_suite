8b95e0ca8a470cf474153fc851a353bd7f022823
Salvador Tamarit <stamarit@dsic.upv.es>
2013-04-02 03:30:39 +0300

    Update happy.erl

--- happy/happy.erl
+++ happy/happy.erl
@@ -11,8 +11,8 @@ happy_list(X, N, L) ->
 	true -> happy_list(X + 1, N, L) end.
  
 is_happy(1) -> true;
-%is_happy(4) -> false;
-is_happy(4) -> true;
+%is_happy(4) -> false; %RIGHT
+is_happy(4) -> true; %WRONG
 is_happy(N) when N > 0 ->
 	N_As_Digits = [Y - 48 || Y <- integer_to_list(N)],
 	is_happy(lists:foldl(fun(X, Sum) -> (X * X) + Sum end, 0, N_As_Digits));
